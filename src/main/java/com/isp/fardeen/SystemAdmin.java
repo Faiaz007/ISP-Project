10bb3c4e15ad4430a16f36cf1d6079f1d9e227e8
Fardeenn Ishraq <fardeen.id78@gmail.com>
2025-04-13 09:15:48 +0600

    "added"

---
+++ src/main/java/com/isp/fardeen/SystemAdmin.java
@@ -0,0 +1,5 @@
+package com.isp.fardeen;
+
+public class SystemAdmin {
+
+}
---
+++ src/main/java/com/isp/fardeen/SystemAdminDashboard.java
@@ -0,0 +1,26 @@
+package com.isp.fardeen;
+
+import javafx.event.ActionEvent;
+
+public class SystemAdminDashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goal3BtnOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal2BtnOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal4BtnOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal1BtnOnClick(ActionEvent actionEvent) {
+    }
+}
