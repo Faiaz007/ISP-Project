0f7b6b0ca4177ba89d07228be3bfd2fcf56bb5a4
Fardeenn Ishraq <fardeen.id78@gmail.com>
2025-04-18 16:35:12 +0600

    updated code

---
+++ src/main/java/com/isp/fardeen/AddNewUser.java
@@ -0,0 +1,26 @@
+package com.isp.fardeen;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddNewUser
+{
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addUserOnAction(ActionEvent actionEvent) {
+    }
+}
